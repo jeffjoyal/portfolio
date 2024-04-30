6e740b481018665e72d14b240f3f7cd77910e2e6
jeffjoyal <jeff.joyal04@gmail.com>
2024-04-29 23:24:03 -0400

    ye

--- src/frontend/components/InputListManager.jsx
+++ src/frontend/components/InputListManager.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+ 
 const InputListManager = () => {
   const [newInput, setNewInput] = useState('');
   const [list, setList] = useState([]);
